deaa49e79b44ca9e1bcd3b8f375c731a8d2b484f
miguel <miguel142v@gmail.com>
2025-02-22 22:21:27 +0100

    List of fruits done

--- fruits-list-app/App.js
+++ fruits-list-app/App.js
@@ -1,20 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Button, TextInput } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  let initialFruits = [
+    { id: '0', name: 'mela' },
+    { id: '1', name: 'arancia' },
+    { id: '2', name: 'banana' },
+  ];
+
+  const [fruit, setFruit] = useState('');
+  const [fruits, setFruits] = useState(initialFruits);
+  const [nextID, setNextID] = useState(initialFruits.length);
+
+
+  return (  // Specifica cosa deve essere renderizzato/visualizzato
+
+    <SafeAreaView style={styles.container}> //Crea un contenitore SafeAreaView che avvolge altri elementi
+      <View style={styles.innerContainer}> //Crea un contenitore View che avvolge altri elementi (simile a un div in HTML)
+        <Text>Fruit list</Text>
+        <View style={styles.rowContainer}>
+          <TextInput backgroundColor='white' width='150'
+            value={fruit}
+            onChangeText={text=> setFruit(text)}>
+            </TextInput>
+          <Button title='Add fruit' backgroundColor='#111' onPress={() => {
+            const newID = nextID;
+            setNextID(nextID + 1);
+            setFruits(
+              [...fruits,
+                {id: newID.toString(),
+                  name: fruit
+                } 
+              ]);
+          }}>
+          </Button>
+        </View>
+        {fruits.map(fruit => (
+          <Text key={fruit.id}>
+             ID: {fruit.id} - Nome: {fruit.name}
+          </Text>
+        ))}
+      </View>
+      <StatusBar style="auto" /> //Barra di stato e auto per il colore di sfondo (auto = backgroundColor)
+    </SafeAreaView>
   );
+
 }
 
-const styles = StyleSheet.create({
+
+//Definizione degli stili
+const styles = StyleSheet.create({ //Simile a CSS ma per React Native
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //contenitore di elementI, occupa tutto lo schermo
+    backgroundColor: '#00ffff', //Colore di sfondo
+  },
+  innerContainer: {
+    flex: 1, //La view occupa tutto lo schermo
+    alignItems: 'center', //Centra gli elementi orizzontalmente
+    justifyContent: 'flex-start', //Centra gli elementi verticalmente
+  },
+  rowContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
